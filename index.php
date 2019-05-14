e6abc9b3ee086190ce9bc35aaa355c7fd713ddc9
avi123nash <avinash.kini@gmail.com>
2019-05-14 09:19:12 -0700

    First Commit - Start Game link

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Farm Feeding Game</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="keywords" content="Farm Feeding Game">
+    </head>
+    <body>
+        <div>
+            <div>
+                <div>
+                    <h2>Farm Feeding Game</h2>
+                    <div>
+                        <a href="">Click here to Start the Game</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
