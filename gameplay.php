cae58a30c445cc34bf2761df5842573e35446bed
avi123nash <avinash.kini@gmail.com>
2019-05-15 08:38:44 -0700

    creating layout of the game

---
+++ gameplay.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<div style="width:100%;text-align:center;" ><h2>Farm Feeding Game</h2></div>
+<form name="ReqFeedForm" id="ReqFeedForm" action="" method="POST" >
+    <input type="submit" name="Submit" id="frm_submit" value="Feed.." />
+    <input type="submit" name="Reload" id="Reload" value="Reload New Game.." />
+</form>
+
+<div><h4>No. Of Feeds</h4></div>
+<div>
+	Farmer Died:
+	Cow Died:
+	Bunny Died:
+</div>
+
+<table>
+    <tr>
+        <td>Farmer</td>
+        <td>Cow1</td>
+        <td>Cow2</td>
+        <td>Bunny1</td>
+        <td>Bunny2</td>
+        <td>Bunny3</td>
+        <td>Bunny4</td>
+    </tr>
+
+</table>
